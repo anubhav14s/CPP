5f7f474a0ed5ac64ca00dcb4f00f780fe1e791f1
Anubhav14s <Anubhav.1406singh@gmail.com>
2025-06-29 16:54:20 +0530

    o

---
+++ overflow.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int x=INT32_MAX;
+    x++;
+    cout<<x;
+    int y=INT32_MIN;
+    y--;
+    cout<<y;
+}
---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int x=INT16_MAX;
+    x++;
+    cout<<x;
+    int y=INT16_MIN;
+    y--;
+    cout<<y;
+}
