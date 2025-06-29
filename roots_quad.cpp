390fc088b689ec49893baf77eee52ec75b1e6546
Anubhav14s <Anubhav.1406singh@gmail.com>
2025-06-29 09:06:24 +0530

    m

---
+++ roots_quad.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main(){
+    int a,b,c;
+    float root,root1;
+    cout<<"Enter the value of a,b and c from the quadratic equation ";
+    cin>>a>>b>>c;
+    root=(-b+sqrt(pow(b,2)-4*a*c))/(2*a);
+    root1=(-b-sqrt(pow(b,2)-4*a*c))/(2*a);
+    cout<<"The two roots of the quadratic equ are "<<root<<","<<root1;
+    return 0;
+}
