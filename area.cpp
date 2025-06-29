d456125e4a25d6333bb71f0f4cc615e23a5b9494
Anubhav14s <Anubhav.1406singh@gmail.com>
2025-06-29 08:51:41 +0530

    m

---
+++ area.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+    float b,h,area;
+    cout<<"Enter base and height";
+    cin>>b>>h;
+    area=b*h/2;
+    cout<<"Area of traingle of base "<<b<<" and height "<<h<<" is "<<area;
+    return 0;
+}
