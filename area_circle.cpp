83c24fa32846fdd7d79928e88f3ac5905fa8c81c
Anubhav14s <Anubhav.1406singh@gmail.com>
2025-06-30 21:05:23 +0530

    m

--- area_circle.cpp
+++ area_circle.cpp
@@ -5,6 +5,6 @@ int main(){
     cout<<"Enter the radius of the circle"<<endl;
     cin>>radius;
     area=3.14*radius*radius;
-    cout<<"The area of circle with radius "<<radius<<" is "<<area<<endl;
+    cout<<"The area of the circle with radius "<<radius<<" is "<<area<<endl;
     return 0;
 }
