25f6c041ff2a20e9db8390cae9b0f9bd929ba008
Anubhav14s <Anubhav.1406singh@gmail.com>
2025-06-30 21:09:59 +0530

    m

--- sum.cpp
+++ sum.cpp
@@ -5,6 +5,6 @@ int main() {
     int x, y;
     cout << "Enter the two numbers: ";
     cin >> x >> y;
-    cout << "Sum is: " << x + y;
+    cout << "Sum of the given number is: " << x + y;
     return 0;
 }
