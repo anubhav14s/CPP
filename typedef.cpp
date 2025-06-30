a3e7f281b984761d0495bf18cd493f176f439120
Anubhav14s <Anubhav.1406singh@gmail.com>
2025-06-30 21:07:02 +0530

    m

--- typedef.cpp
+++ typedef.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 using namespace std;
 typedef int marks;
-typedef char grade;
+typedef char grades;
 int main(){
     marks m1,m2;
-    grade a,b,c;
+    grades a,b,c;
     m1=34;
     m2=78;
     a='A';
