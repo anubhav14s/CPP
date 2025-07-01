cc64dd853bd6673242d08691d336bcb0f78a45c1
Anubhav14s <Anubhav.1406singh@gmail.com>
2025-07-01 15:43:57 +0530

    g

---
+++ nested_if.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std ;
+int main(){
+    int a,b,c;
+    cout<<"Enter the three numbers ";
+    cin>>a>>b>>c;
+    if(a>b&&a>c){
+        cout<<"The greatest number is a "<<a<<endl;}
+        else{
+            if(b>c){
+                
+                cout<<"The greatest number is b "<<b<<endl;
+            }
+            else {
+            cout<<"The greatest number is c "<<c<<endl;
+
+        }
+    }
+}
