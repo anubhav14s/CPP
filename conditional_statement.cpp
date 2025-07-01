75c1e8f8000c6c350f1f06243e5e3e8b666874ec
Anubhav14s <Anubhav.1406singh@gmail.com>
2025-07-01 14:31:53 +0530

    m

---
+++ conditional_statement.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using  namespace std;
+int main(){
+    int num;
+    cout<<"Enter a number";
+    cin>>num;
+    if(num%2==0){
+        cout<<"The number entered is an even number";
+    }else{
+
+        cout<<"The number entered is an odd number";
+    }
+    return 0;
+    
+}
