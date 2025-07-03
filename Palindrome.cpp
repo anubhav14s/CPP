41f728df2e0976a685854572ed47026501081b1a
Anubhav14s <Anubhav.1406singh@gmail.com>
2025-07-03 15:18:41 +0530

    m

---
+++ Palindrome.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num, rev = 0, rem = 0;
+    cout << "Enter the number" << endl;
+    cin >> num;
+    int original=num;
+    while (num != 0)
+    {
+        rem = num % 10;
+        rev = rev * 10 + rem;
+        num=num/10;
+    }
+    if(rev==original){
+        cout<<original<<" is a Palindrome";
+    }else{
+
+        cout<<original<<" is not a Palindrome";
+    }
+}
