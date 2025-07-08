0796f68e38e87a0e44d3836ed4a3eb0e1a8dcf7e
Anubhav14s <Anubhav.1406singh@gmail.com>
2025-07-08 20:05:47 +0530

    m

--- Refrence.cpp
+++ Refrence.cpp
@@ -3,10 +3,13 @@ using namespace std;
 int main(){
     int x=34;
     int &y=x;
+    cout<<"Y is the refrence of X"<<endl;
     cout<<x<<endl;
     x++;
     y++;
+
     cout<<"The value of x is "<<x<<endl;
     cout<<"The value of y is "<<y<<endl;
+    
     
 }
