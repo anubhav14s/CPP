82cbab804d330c326f08aa86afb9127bee38eb24
Anubhav14s <Anubhav.1406singh@gmail.com>
2025-07-08 19:23:27 +0530

    m

--- Dynamic_memory_allocation.cpp
+++ Dynamic_memory_allocation.cpp
@@ -1,12 +1,22 @@
 #include <iostream>
 using namespace std;
 int main(){
-    cout<<"The array is created in heap"<<endl;
-    int *p= new int[10];
-    cout<<sizeof p<<endl;
+    cout<<"Enter the size of the array"<<endl;
+    int size;
+    cin>>size;
+    cout<<endl;
+    int *p= new int[size];
+    cout<<"The array of size "<<size* sizeof (int)<<" is created in heap"<<endl;
+    
+    delete []p;
     cout<<"Size of the array can be changed after creating it."<<endl;
 
+    cout<<"Enter the size of the array again"<<endl;
+    cin>>size;
+    
     p=new int[24];
-    cout<<sizeof p;
+    cout<<"The array  size is changed to "<<size* sizeof (int)<<endl;
+
+    delete []p;
 
 }
