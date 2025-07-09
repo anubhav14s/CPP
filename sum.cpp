6436d9f73afee189c43887085cc7db82fedf1d89
Anubhav14s <Anubhav.1406singh@gmail.com>
2025-07-09 21:38:55 +0530

    m

--- sum.cpp
+++ sum.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main() {
     int x, y;
-    cout << "Enter the two numbers: ";
+    cout << "Enter the two number: ";
     cin >> x >> y;
     cout << "Sum of the given numbers is: " << x + y;
     return 0;
