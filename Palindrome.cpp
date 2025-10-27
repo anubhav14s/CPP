b326fbef20c5c6e48718d290a327c15d7d3fd201
Anubhav singh <anubhav.1406singh@gmail.com>
2025-10-27 14:23:36 +0530

    m

--- Palindrome.cpp
+++ Palindrome.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main()
 {
     int num, rev = 0, rem = 0;
-    cout << "Enter the number" << endl;
+    cout << "Enter a number" << endl;
     cin >> num;
     int original=num;
     while (num != 0)
