587b412a67fc92af829e97cd31af7afd244696d4
Anubhav singh <anubhav.1406singh@gmail.com>
2025-11-18 11:53:39 +0530

    m

---
+++ pointer_to_object.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+class Rectangle{
+public:
+    int length;
+    int breadth;
+    int area(){
+        return length*breadth;
+    }
+    int perimeter(){
+        return 2*(length+breadth);
+    }
+};
+int main(){
+    Rectangle *ptr=new Rectangle;
+    ptr -> length=21;
+    ptr -> breadth=14;
+cout<<"Area is "<<ptr->area()<<endl;
+cout<<"Perimeter is "<<ptr->perimeter()<<endl;
+return 0;
+}
