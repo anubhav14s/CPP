57e2272546714865942dec3509c29ad011a26bec
Anubhav singh <anubhav.1406singh@gmail.com>
2025-12-01 11:53:34 +0530

    m

--- Strings/string.cpp
+++ Strings/string.cpp
@@ -4,8 +4,8 @@ using namespace std;
 int main(){
     char str[50];
     cout<<"enter a string "<<endl;
-    cout<<str<<endl;
     cin.getline(str,50);
+    cout<<str<<endl;
     cout<<"Length of the given string is "<<strlen(str)<<endl;
     char *s ="good moring ";
     strcpy(str,s);
