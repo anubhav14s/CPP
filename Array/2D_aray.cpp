9f0297c614ae58d5fc642ddad98155c115c26ff4
Anubhav singh <anubhav.1406singh@gmail.com>
2025-12-01 21:08:37 +0530

    m

--- Array/2D_aray.cpp
+++ Array/2D_aray.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 int main(){
-    int A[2][3]={{13,8,9},{3,5,82}};
+    int A[2][3]={{13,8,9},{3,7,82}};
     int B[3][2]={3,9,6,1,6,3};
     for(int i=0;i<2;i++){
         for(int j=0;j<3;j++){
