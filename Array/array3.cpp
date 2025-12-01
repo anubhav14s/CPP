d2abf0d575a46f8eb530294f46a6c5f4cb51977a
Anubhav singh <anubhav.1406singh@gmail.com>
2025-12-01 21:10:05 +0530

    m

--- Array/array3.cpp
+++ Array/array3.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 int main(){
-    int A[7]={7,19,5,98,45,67,999},max=0;
+    int A[7]={7,19,5,98,45,67,989},max=0;
     for(int x:A)
     if(x>max){
         max=x;
